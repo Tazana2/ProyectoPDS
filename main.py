107ff4ea7eb1cae0c3ea16e6ff9b3b1941e78496
Tazana2 <danisanmez@gmail.com>
2023-02-22 09:32:40 -0500

    Cambios en la forma del reconocimiento

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import face_recognition
 import cv2
 
+daniel = cv2.imread("Faces/Daniel.jpg")
+samuel = cv2.imread("Faces/Samuel.jpg")
+faces = [daniel, samuel]
+names = ["Daniel", "Samuel"]
 
-# Abrir la imagen y encontrar las cordenadas de un solo rostro
-myImage = cv2.imread("Faces/Daniel.jpg")
-faceCor = face_recognition.face_locations(myImage)[0]
-
-# Tomar las caracteristicas del rostro dentro de la carpeta en un Array
-faceEncoImg = face_recognition.face_encodings(myImage, known_face_locations=[faceCor])[0]
+# felipe = cv2.imread("Faces/Felipe.jpg")
+savedFaces = []
+for i in faces:
+    savedFaces.append(face_recognition.face_encodings(i, known_face_locations=[face_recognition.face_locations(i)[0]])[0])
 
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -24,21 +26,20 @@ while True:
     # A cada rostro encontrado se le toman sus caracteristicas y se le hace un rectangulo en donde se encuentre
     if faceCor != []:
         for eachFace in faceCor:
-            faceEnco = face_recognition.face_encodings(frame, known_face_locations=[eachFace])[0]
+            faceEnco = face_recognition.face_encodings(frame, known_face_locations=[eachFace])
             # Comparar el Frame con el rostro de la imagen
-            match = face_recognition.compare_faces([faceEnco], faceEncoImg)
-            # print(f"El rostro es el mismo que el de la imagen?: {match}")
-
-            if match[0] == True:
-                myText = "Daniel"
-                myColor = (0, 255, 0)
-            else:
-                myText = "Unknown"
-                myColor = (0, 0, 255)
+            myText = "Unknow"
+            myColor = (0, 0, 255)
+            for face in faceEnco:
+                match = face_recognition.compare_faces(savedFaces, face)
+                if True in match:
+                    name = names[match.index(True)]
+                    myText = name
+                    myColor = (0, 255, 0)
 
             # cv2.rectangle(frame, (eachFace[3], eachFace[0]), (eachFace[1], eachFace[2] + 30), myColor, -1)
-            cv2.rectangle(frame, (eachFace[3], eachFace[0]), (eachFace[1], eachFace[2]), myColor, 2)
-            cv2.putText(frame, myText, (eachFace[3],eachFace[2]+ 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 1)
+            # cv2.rectangle(frame, (eachFace[3], eachFace[0]), (eachFace[1], eachFace[2]), myColor, 2)
+            cv2.putText(frame, myText, (eachFace[3],eachFace[2]+ 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, myColor, 2)
 
 
     cv2.imshow("a", frame)
