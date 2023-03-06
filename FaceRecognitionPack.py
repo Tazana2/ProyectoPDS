587ea0c9de87b6c95c574a21e45adaed56813de6
Tazana2 <danisanmez@gmail.com>
2023-03-06 14:02:08 -0500

    Se integra el módulo de reconocimiento facial con la interfaz

---
+++ FaceRecognitionPack.py
@@ -0,0 +1,34 @@
+import face_recognition
+import cv2
+import os
+
+class FaceRecognition:
+    def __init__(self, path="", names=[], savedFaces=[], cap=None):
+        self.names = names
+        self.path = path
+        self.savedFaces = savedFaces
+        self.cap = cap
+
+
+    def save_info(self):
+        self.names = [name[:name.index('.')] for name in os.listdir(self.path)]
+        faces = [cv2.imread(os.path.join(self.path, image)) for image in os.listdir(self.path)]
+        self.savedFaces = [face_recognition.face_encodings(i, known_face_locations=[face_recognition.face_locations(i)[0]], model="small")[0] for i in faces]
+
+    def start_recognition(self):
+        # Ingresar los frames de la cámara principal del dispositivo
+        ret, frame = self.cap.read()
+        if ret == False: return IndexError
+        frame = cv2.flip(frame, flipCode=1)
+        # Encontrar las coordenadas de todos los rostros presentes
+        faceCor = face_recognition.face_locations(frame)
+        # A cada rostro encontrado se le toman sus caracteristicas y se le hace un rectangulo en donde se encuentre
+        if faceCor != []:
+            for eachFace in faceCor:
+                faceEnco = face_recognition.face_encodings(frame, known_face_locations=[eachFace], model="small")
+                for face in faceEnco:
+                    match = face_recognition.compare_faces(self.savedFaces, face)
+                    if True in match:
+                        # Acá es donde va a estar el espacio para la autenticación
+                        myText = self.names[match.index(True)]
+                        return myText
--- main.py
+++ main.py
@@ -1,57 +1,187 @@
-import face_recognition
+from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
+from customtkinter import *
 import cv2
-import os
-
-class FaceRecognition:
-    def __init__(self, path="", names=[], savedFaces=[]):
-        self.names = names
-        self.path = path
-        self.savedFaces = savedFaces
 
 
-    def save_info(self):
-        self.names = [name[:name.index('.')] for name in os.listdir(self.path)]
-        faces = [cv2.imread(os.path.join(self.path, image)) for image in os.listdir(self.path)]
-        self.savedFaces = [face_recognition.face_encodings(i, known_face_locations=[face_recognition.face_locations(i)[0]], model="small")[0] for i in faces]
-
-    def start_recognition(self):
-        cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-        while True:
-            # Ingresar los frames de la cámara principal del dispositivo
-            ret, frame = cap.read()
-            if ret == False: break
-            frame = cv2.flip(frame, flipCode=1)
-
-            # Encontrar las coordenadas de todos los rostros presentes
-            faceCor = face_recognition.face_locations(frame)
-
-            # A cada rostro encontrado se le toman sus caracteristicas y se le hace un rectangulo en donde se encuentre
-            if faceCor != []:
-                for eachFace in faceCor:
-                    faceEnco = face_recognition.face_encodings(frame, known_face_locations=[eachFace], model="small")
-                    myText = "Unknow"
-                    myColor = (0, 0, 255)
-                    for face in faceEnco:
-                        match = face_recognition.compare_faces(self.savedFaces, face)
-                        if True in match:
-                            # Acá es donde va a estar el espacio para la autenticación
-
-                            myText = self.names[match.index(True)]
-                            myColor = (0, 255, 0)
-                    cv2.putText(frame, myText, (eachFace[3],eachFace[2]+ 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, myColor, 2)
+from FaceRecognitionPack import FaceRecognition
 
 
-            cv2.imshow("Authentication", frame)
+class App(CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Face Recoginition Project")
+        self.geometry("600x600")
+        self.bind('<Escape>', lambda e: self.quit())
+        self.vid = None
 
-            k = cv2.waitKey(1)
-            if  k == 27 & 0xFF:
-                break
+        self.frame = CTkFrame(self)
+        self.frame.pack(pady=40, padx=50, fill="both", expand=True)
 
-        cap.release()
-        cv2.destroyAllWindows()
+        self.main_title = CTkLabel(self.frame, text="¡Bienvenido!", font=("Monocraft", 28)).pack(pady=20)
+
+        self.container_register = CTkFrame(self.frame)
+        self.container_register.pack(pady=15, padx=20, fill="both", expand=True)
+
+        self.container_login = CTkFrame(self.frame)
+        self.container_login.pack(pady=15, padx=20, fill="both", expand=True)
 
 
-m = FaceRecognition(".\\Faces")
+        self.label1 = CTkLabel(self.container_register, text="Registrate", font=("Monocraft", 20))
+        self.label1.pack(pady=25)
 
-m.save_info()
-m.start_recognition()
+        self.nameText = Variable()
+        self.entry_name = CTkEntry(
+            self.container_register, 
+            textvariable=self.nameText, 
+            placeholder_text="Escribe tu nombre", 
+            font=("Monocraft", 12)
+        )
+        self.entry_name.pack(padx=100, fill="x")
+
+        self.button_register = CTkButton(
+            self.container_register,
+            text="Registrate",
+            font=("Monocraft", 12),
+            command=self.checkNameEntry
+        )
+        self.button_register.pack(pady=10)
+
+
+        self.label2 = CTkLabel(self.container_login, text="Iniciar Sesión", font=("Monocraft", 20))
+        self.label2.pack(pady=30)
+
+        self.button_login = CTkButton(self.container_login, text="Ingresar", font=("Monocraft", 12), command=self.bruh)
+        self.button_login.pack()
+
+
+    def createTopLevel(self, whatIsfor=""):
+        self.nwWindow = CTkToplevel(self)
+        self.nwWindow.title("Authentication")
+        self.nwWindow.geometry("550x550")
+        if whatIsfor == "Register":
+            self.registerWin()
+        elif whatIsfor == "Login":
+            self.loginWin()
+
+    def closeTopLevel(self):
+        self.entry_name.delete(0, END)
+        self.nwWindow.destroy()
+        self.nwWindow.update()
+        if self.vid:
+            self.vid.release()
+
+    def openCamera(self):
+        cv_image = cv2.flip(cv2.cvtColor(self.vid.read()[1], cv2.COLOR_BGR2RGBA), 1)
+        captured_image = Image.fromarray(cv_image)
+        photo_image = ImageTk.PhotoImage(image=captured_image)
+
+        self.cam_widget.photo_image = photo_image
+        self.cam_widget.configure(image=photo_image)
+        self.cam_widget.after(10, self.openCamera)
+
+
+    def take_photo_register(self):
+        try:
+            cv2.imwrite(f".\Faces\{self.nameText.get()}.jpg", cv2.flip(self.vid.read()[1], 1))
+            self.closeTopLevel()
+            messagebox.showinfo(
+                message="Te has registrado con éxito! :D",
+                title="Registro completado"
+            )
+        except:
+            self.closeTopLevel()
+            messagebox.showerror(
+                message="Ha ocurrido al registrate. Revisa tu cámara o el campo del nombre (Recuerda que no puede tener cáracteres especiales) y vuelve a intentar D:" ,
+                title="Error:"
+            )
+
+    def registerWin(self):
+        try:
+            self.vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+            self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
+            self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
+        except:
+            self.closeTopLevel()
+            messagebox.showerror(
+                message="Ha ocurrido un error al detectar tu dispositivo de video. Revisalo y vuelve a intentar.",
+                title="Error:"
+            )
+
+        self.cam_widget = Label(self.nwWindow)
+        self.cam_widget.pack(pady=20, padx=20)
+
+        capBtn = CTkButton(self.nwWindow, text="Tomar Foto", command=self.take_photo_register, font=("Monocraft", 12))
+        capBtn.pack(pady=10)
+
+        clBtn = CTkButton(self.nwWindow, text="Salir", command=self.closeTopLevel, font=("Monocraft", 12))
+        clBtn.pack()
+        self.openCamera()
+
+    def loginWin(self):
+        try:
+            self.vid = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+            self.vid.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
+            self.vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
+        except:
+            self.closeTopLevel()
+            messagebox.showerror(
+                message="Ha ocurrido un error al detectar tu dispositivo de video. Revisalo y vuelve a intentar.",
+                title="Error:"
+            )
+
+        self.cam_widget = Label(self.nwWindow)
+        self.cam_widget.pack(pady=20, padx=20)
+
+        label_recomendation = CTkLabel(self.nwWindow, text="Recuerda:\n\nEs mejor si estás en un lugar iluminado \ny con nadie más que tú en la cámara.", font=("Monocraft", 14))
+        label_recomendation.pack(pady=10)
+
+        capBtn = CTkButton(self.nwWindow, text="Iniciar Reconocimiento", command=self.startAuthentication, font=("Monocraft", 12))
+        capBtn.pack(pady=10)
+
+        clBtn = CTkButton(self.nwWindow, text="Salir", command=self.closeTopLevel, font=("Monocraft", 12))
+        clBtn.pack()
+
+        self.openCamera()
+
+    def checkNameEntry(self):
+        if self.nameText.get() != "": self.createTopLevel(whatIsfor="Register")
+        else:
+            messagebox.showerror(
+                message="Tienes que ingresar toda la información para registrarte",
+                title="Error:"
+            )
+
+    def startAuthentication(self):
+        try:
+            m = FaceRecognition(path=".\\Faces", cap=self.vid)
+            m.save_info()
+            name = m.start_recognition()
+            if name in m.names:
+                self.closeTopLevel()
+                messagebox.showinfo(
+                    message=f"Bienvenido {name}",
+                    title="Inicio de sesión exitoso"
+                )
+            else:
+                self.closeTopLevel()
+                messagebox.showerror(
+                    message="Usted no está registrado o no se le pudo reconocer",
+                    title="Error:"
+                )
+        except:
+            self.closeTopLevel()
+            messagebox.showerror(
+                message="Ha ocurrido un error al realizar la autenticación",
+                title="Error:"
+            )
+
+    def bruh(self):
+        self.createTopLevel(whatIsfor="Login")
+
+
+
+if __name__ == "__main__":
+    root = App()
+    root.mainloop()
